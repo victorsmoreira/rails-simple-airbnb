58823cbd233448176b2e4d7a4a6898e8f618f2ed
Victor Moreira <victorsmoreira@outlook.com>
2021-08-18 17:03:23 -0300

    index done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+4.times do
+  Flat.create!(
+    name: Faker::Coffee.blend_name,
+    address: Faker::Address.full_address,
+    description: Faker::GreekPhilosophers.quote,
+    price_per_night: rand(75..1500),
+    number_of_guests: rand(1..20)
+  )
+end
